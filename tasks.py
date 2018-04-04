96f03ce22842d23bdda2e0bed5906b1832fb9a37
Gustavo G. Ribeiro <gribeiro@esss.co>
2018-04-04 10:09:16 -0300

    Add invoke tasks for build C++ version of HelloWorld

---
+++ tasks.py
@@ -0,0 +1,29 @@
+from invoke import task 
+
+@task()
+def clean(ctx):
+	"""
+	Delete 'build' folder.
+	"""
+	print("Cleaning!")
+	ctx.run("rm -Rf build")
+
+@task(
+	help = {
+		'cclean': "Call Clean task (Delete 'build' folder) before build again."
+	}
+)
+def build(ctx, cclean=False):
+	"""
+	Build C++ code.
+	"""
+	if cclean:
+		clean(ctx)
+
+	print("Building!")
+	commands = [
+		'mkdir build',
+        'cd build',
+        'cmake ..',
+    ]
+	ctx.run(' && '.join(commands))
